bdb4e172724aca84243fbf50ed7d6a8511d99a19
Ryohei <gawryomyo@gmail.com>
2022-12-20 16:04:44 +0700

    test

---
+++ src/test/java/com/consol/citrus/samples/SampleJavaIT.java
@@ -0,0 +1,40 @@
+package com.consol.citrus.samples;
+
+import org.springframework.core.io.ClassPathResource;
+import org.testng.annotations.Test;
+
+import com.consol.citrus.annotations.CitrusTest;
+import com.consol.citrus.dsl.testng.TestNGCitrusTestDesigner;
+
+
+@Test
+public class SampleJavaIT extends TestNGCitrusTestDesigner {
+
+
+    @CitrusTest(name = "SendMessageTest")
+    public void sendMessageTest() { //готово
+
+        description("Basic send message example");
+
+        variable("text", "Hello Citrus!");
+        variable("messageId", "Mx1x123456789");
+
+        send("helloServiceEndpoint")
+                .name("helloMessage")
+                .payload(new ClassPathResource("com/consol/citrus/samples/message.xml"))
+                .header("Operation", "sayHello")
+                .header("RequestTag", "${messageId}");
+    }
+
+    @CitrusTest(name = "SampleJavaTest.receiveMessageTest")
+    public void receiveMessageTest() {
+
+
+        receive("helloServiceEndpoint")
+                .validate("bookstore//book[last()]/date", "17.12.22")
+                .validate("/bookstore/book/title[@lang='fr']","Notre-Dame de Paris" );
+                //.xsd("testSchema");
+
+
+    }
+}
